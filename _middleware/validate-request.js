84f608dfafedf51f903f7fd96d9d77875f70efdc
damporjohn <damporjohn233@gmail.com>
2025-03-09 21:15:14 +0800

    all fixed

--- _middleware/validate-request.js
+++ _middleware/validate-request.js
@@ -1,16 +1,18 @@
-module.exports - validateRequest;
+module.exports = validateRequest;
 
 function validateRequest(req, next, schema) {
     const options = {
         abortEarly: false, // include all errors
-        allowUnknown: true, // ignore unknown props
-        stripUnknown: true // remove unknown props
+        allowUnknown: true, // ignore unknown properties
+        stripUnknown: true // remove unknown properties
     };
+
     const { error, value } = schema.validate(req.body, options);
+    
     if (error) {
-        next(`Validation error: ${error.details.map(x => x.message). join(', ')}`);
+        next(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
     } else {
         req.body = value;
-        next ();
+        next();
     }
 }
